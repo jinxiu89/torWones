7a067d7ae2f8f63075c5b60c3b3baccd9dd471cf
thomas <jinxiu89@163.com>
2019-03-03 08:23:34 +0800

    结构优化

--- handlers/passport/accounts/authHandlers.py
+++ handlers/passport/accounts/authHandlers.py
@@ -3,7 +3,9 @@
 # author:jinxiu89@163.com
 # create by thomas on 18-12-31.
 from handlers.base import BaseHandler
+from libs.captcha.capthca import auth_captcha
 from forms.passport import login
+
 
 class AuthHandler(BaseHandler):
     def get(self, *args, **kwargs):
@@ -11,15 +13,26 @@ class AuthHandler(BaseHandler):
 
 
 class LoginHandler(BaseHandler):
+    def data_received(self, chunk):
+        pass
+
     def get(self, *args, **kwargs):
-        kwargs={
-            "msg":"用户登陆",
-            "form":login.LoginForm()
+        kwargs = {
+            "msg": "用户登陆",
+            "form": login.LoginForm()
         }
         self.render('passport/accounts/auth/login.html', **kwargs)
 
     def post(self, *args, **kwargs):
-        pass
+        data = self.json.loads(self.request.body)
+        result = auth_captcha(self, data['captcha'])
+        if result['status'] is False:
+            return self.write({"status": False, "message": result['msg']})
+        elif result['status'] is True:
+            if result['status'] is False:
+                return self.write({"status": False, "message": result['msg']})
+            else:
+                return self.write({"status": True, "message": result['msg']})
 
 
 class SignUpHandler(BaseHandler):
---
+++ static/libs/js/commons.js
@@ -0,0 +1,52 @@
+/**
+ * create by Administrator on 16-7-16
+ * 处理 Post数据
+* 提交form表单数据
+* #button_add :提交按钮的ID
+* #form-data:提交表单的form ID
+* 利用serializeArray
+* 组装数组用each方法
+* */
+$("#button_add").click(function () {
+    var data = $("#form-data").serializeArray();
+    /***
+     * 修正日期：2016-09-08
+     * 修正人：袁普照
+     * 解决数组的问题
+     */
+    var postData={};
+    $(data).each(function(){
+        if(postData[this.name]){
+            if($.isArray(postData[this.name])){
+                postData[this.name].push(this.value);
+            }else{
+                postData[this.name]=[postData[this.name],this.value];
+            }
+        }else{
+            postData[this.name]=this.value;
+        }
+    });
+
+    //console.log(postData);
+    //SCOPE是在模板文件的script block中写的
+    var url=SCOPE.save_url;
+    //noinspection JSUnresolvedFunction
+    $.post(url,postData,function (result) {
+        //r如果返回结果是0 表示失败了
+        if(result.status == 0){
+            dialog.error(result.message);
+        }
+        // 如果返回结果为1 则表示成功
+        if(result.status == 1){
+            dialog.success(result.message,result.jump_url);
+        }
+    },'JSON');
+});
+
+
+//获取cookie
+function get_cookie(name) {
+    var cookies;
+    cookies = document.cookie.match("\\b" + name + "=([^;]*)\\b");
+    return cookies[1];
+}
